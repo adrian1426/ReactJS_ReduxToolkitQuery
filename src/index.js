549e2da8768a71ddc553d9cfe5067db333a86472
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2022-11-29 21:16:11 -0600

    configurando store de redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import React, { StrictMode } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from './store';
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <StrictMode>
+  <Provider store={store}>
     <App />
-  </StrictMode>
+  </Provider>
 );
